876fe2f1ebbc00afdbc494d16d9d364438044d82
Max Hayashi Batista <rm563717@fiap.com.br>
2025-10-06 09:27:43 -0300

    atualiza texto do pedido

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <h1>Pizzaria Gamma Coral</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Qauntas pizzas você quer? {count}
+          Qauntas pizzas você vai querer? {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
